cfc45acc711d06154bd9076d79e8f59367b63c0f
ka <insideka01@gmail.com>
2022-03-14 08:51:37 +0900

    03.14 Jetbrain - Standard input

---
+++ Topics/Standard input/Read and print/src/Main.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val string1 = scanner.nextLine()
+    val string2 = scanner.nextLine()
+    val int1 = scanner.nextInt()
+    val int2 = scanner.nextInt()
+
+    println(string1)
+    println(string2)
+    println(int1)
+    println(int2)
+}
