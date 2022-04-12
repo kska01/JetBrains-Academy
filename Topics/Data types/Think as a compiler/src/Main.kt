092c791d193fc0267160bbc0587664ab651c88f3
ka <insideka01@gmail.com>
2022-04-12 13:29:55 +0900

    04.12 JetBrain Kotlin - Think as a compiler

---
+++ Topics/Data types/Think as a compiler/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    val first: String = "ks"
+    val last: String = "ka"
+    val age: Int = 40
+    // put your code above
+    println("My name is $first $last and I’m $age years old")
+}
