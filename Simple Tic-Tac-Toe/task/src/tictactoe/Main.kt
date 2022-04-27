26ca50649b34bb228b18acf243779f5c04202e58
ka <insideka01@gmail.com>
2022-04-27 15:36:28 +0900

    04.27 JetBrains - Work on project. Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,7 +1,13 @@
 package tictactoe
 
 fun main() {
-    println("X O X")
-    println("O X O")
-    println("X X O")
+    val str = readln()
+    val charArray = str.toCharArray()
+    println("""
+        ---------
+        | ${charArray[0]} ${charArray[1]} ${charArray[2]} |
+        | ${charArray[3]} ${charArray[4]} ${charArray[5]} |
+        | ${charArray[6]} ${charArray[7]} ${charArray[8]} |
+        ---------
+        """.trimIndent())
 }
