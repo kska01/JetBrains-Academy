1aa5e02b47e799a2d24c7f5a4b05067df0c08c6d
ka <insideka01@gmail.com>
2022-05-10 16:57:53 +0900

    05.10

---
+++ Topics/Ranges/Within both ranges/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val range1 = readln().toInt()..readln().toInt()
+    val range2 = readln().toInt()..readln().toInt()
+    val value = readln().toInt()
+    println(value in range1 && value in range2)
+}
