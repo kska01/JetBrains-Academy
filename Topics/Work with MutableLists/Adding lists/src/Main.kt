d2763bf0dfb7ffdc4c18e5fc751ef1baaa425631
ka <insideka01@gmail.com>
2022-05-11 21:45:01 +0900

    05.11

---
+++ Topics/Work with MutableLists/Adding lists/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val firstList = readLine()!!.split(' ').map { it }.toMutableList()
+    val secondList = readLine()!!.split(' ').map { it }.toMutableList()
+    println((firstList + secondList).joinToString())
+}
