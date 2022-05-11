bfebb9e17c6c43e8cb8a3edb4c5643313a30eb0e
ka <insideka01@gmail.com>
2022-05-11 11:49:16 +0900

    05.11

---
+++ Topics/If expression/Positive, Negative or Zero/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val n = readln().toInt()
+    if (n > 0) {
+        println("positive")
+    } else if (n < 0) {
+        println("negative")
+    } else {
+        println("zero")
+    }
+}
