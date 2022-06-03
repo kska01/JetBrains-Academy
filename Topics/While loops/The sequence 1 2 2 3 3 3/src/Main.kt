0c2eaaef5c03a74e62811d151456d171433c93ff
ka <insideka01@gmail.com>
2022-06-03 23:59:21 +0900

    06.03

---
+++ Topics/While loops/The sequence 1 2 2 3 3 3/src/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val input = readln().toInt()
+    var num = 1
+    var count = 0
+    while (num <= input) {
+        var subNum = 0
+        while (subNum < num) {
+            print("$num ")
+            subNum++
+            count++
+            if (count == input) break
+        }
+        num++
+        if (count == input) break
+    }
+}
