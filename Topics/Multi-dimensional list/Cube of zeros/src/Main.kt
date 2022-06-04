b9d2e8aac0e827e698df1367d6a2dda193da3ca4
ka <insideka01@gmail.com>
2022-06-05 01:04:03 +0900

    06.05

---
+++ Topics/Multi-dimensional list/Cube of zeros/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val list = listOf(
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0)),
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0)),
+        listOf(listOf(0, 0, 0), listOf(0, 0, 0), listOf(0, 0, 0))
+    )
+    println(list)
+}
