3fe6216f01565ae9bac632e0f0339da37aca285b
ka <insideka01@gmail.com>
2022-07-04 19:47:57 +0900

    07.04

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -3,94 +3,119 @@ package tictactoe
 import kotlin.math.abs
 
 fun main() {
-//    print("Enter cells:")
-//    val str = readln().uppercase()
-//    val arr = str.toCharArray()
-//    var x = 0
-//    var o = 0
-//    var xCount = 0
-//    var oCount = 0
-//    println("""
-//        ---------
-//        | ${arr[0]} ${arr[1]} ${arr[2]} |
-//        | ${arr[3]} ${arr[4]} ${arr[5]} |
-//        | ${arr[6]} ${arr[7]} ${arr[8]} |
-//        ---------
-//        """.trimIndent())
-//    for (v in 0..6 step 3) {
-//        if (arr[v] == 'X' && arr[v + 1] == 'X' && arr[v + 2] == 'X') x = 1
-//        if (arr[v] == 'O' && arr[v + 1] == 'O' && arr[v + 2] == 'O') o = 1
-//    }
-//    for (v in 0..2) {
-//        if (arr[v] == 'X' && arr[v + 3] == 'X' && arr[v + 6] == 'X') x = 1
-//        if (arr[v] == 'O' && arr[v + 3] == 'O' && arr[v + 6] == 'O') o = 1
-//    }
-//    for (v in 0..8) {
-//        if (arr[v] == 'X') xCount++
-//        if (arr[v] == 'O') oCount++
-//    }
-//    if (arr[0] == 'X' && arr[4] == 'X' && arr[8] == 'X') x =1
-//    if (arr[0] == 'O' && arr[4] == 'O' && arr[8] == 'O') o = 1
-//    if (arr[2] == 'X' && arr[4] == 'X' && arr[6] == 'X') x =1
-//    if (arr[2] == 'O' && arr[4] == 'O' && arr[6] == 'O') o = 1
-//
-//    println(
-//        if (x == 1 && o == 1 && arr.contains('_') || abs(xCount - oCount) >= 2) {
-//            "Impossible"
-//        } else if (x == 0 && o == 0 && arr.contains('_')) {
-//            "Game not finished"
-//        } else if (x == 1 && o == 0 ) {
-//            "X wins"
-//        } else if (x == 0 && o == 1) {
-//            "O wins"
-//        } else {
-//            "Draw"
-//        }
-//    )
+    println(
+        """
+        ---------
+        |       |
+        |       |
+        |       |
+        ---------
+        """.trimIndent()
+    )
 
-    print("Enter cells:")
-    val str = readln().uppercase()
-    val list = str.toMutableList()
+    var row = ""
+    var col = ""
+    var r = 0
+    var c = 0
+    var count = 0
+
+    val grid = MutableList(3) { MutableList(3) { ' ' } }
+
+    do {
+        do {
+            print("Enter the coordinates: ")
+            val input = readln()
+            row = input.split(' ').first()
+            col = input.split(' ').last()
+
+            if (row in "0123456789" && col in "0123456789") {
+                if (row in "123" && col in "123") {
+                    r = row.toInt()
+                    c = col.toInt()
+                    if (grid[r - 1][c - 1] == 'X' || grid[r - 1][c - 1] == 'O') {
+                        println("This cell is occupied! Choose another one!")
+                    }
+                } else {
+                    println("Coordinates should be from 1 to 3!")
+                }
+            } else {
+                println("You should enter numbers!")
+            }
+
+
+        } while (r !in 1..3 || c !in 1..3 || grid[r - 1][c - 1] == 'X' || grid[r - 1][c - 1] == 'O')
+
+        r -= 1
+        c -= 1
+        count += 1
+
+        if (count % 2 == 0) {
+            grid[r][c] = 'O'
+        } else {
+            grid[r][c] = 'X'
+        }
+
+        println(
+            """
+        ---------
+        | ${grid[0][0]} ${grid[0][1]} ${grid[0][2]} |
+        | ${grid[1][0]} ${grid[1][1]} ${grid[1][2]} |
+        | ${grid[2][0]} ${grid[2][1]} ${grid[2][2]} |
+        ---------
+        """.trimIndent()
+        )
+    } while (checkResult(grid) != "X wins" && checkResult(grid) != "O wins" && checkResult(grid) != "Draw"
+        && checkResult(grid) != "Impossible"
+    )
+
+    println(checkResult(grid))
+}
+
+fun checkResult(grid: MutableList<MutableList<Char>>): String {
     var x = 0
     var o = 0
     var xCount = 0
     var oCount = 0
     var result = ""
-    println("""
-        ---------
-        | ${list[0]} ${list[1]} ${list[2]} |
-        | ${list[3]} ${list[4]} ${list[5]} |
-        | ${list[6]} ${list[7]} ${list[8]} |
-        ---------
-        """.trimIndent())
-    for (v in 0..6 step 3) {
-        if (list[v] == 'X' && list[v + 1] == 'X' && list[v + 2] == 'X') x = 1
-        if (list[v] == 'O' && list[v + 1] == 'O' && list[v + 2] == 'O') o = 1
-    }
-    for (v in 0..2) {
-        if (list[v] == 'X' && list[v + 3] == 'X' && list[v + 6] == 'X') x = 1
-        if (list[v] == 'O' && list[v + 3] == 'O' && list[v + 6] == 'O') o = 1
-    }
-    for (v in 0..8) {
-        if (list[v] == 'X') xCount++
-        if (list[v] == 'O') oCount++
-    }
-    if (list[0] == 'X' && list[4] == 'X' && list[8] == 'X') x =1
-    if (list[0] == 'O' && list[4] == 'O' && list[8] == 'O') o = 1
-    if (list[2] == 'X' && list[4] == 'X' && list[6] == 'X') x =1
-    if (list[2] == 'O' && list[4] == 'O' && list[6] == 'O') o = 1
 
-    if (x == 1 && o == 1 && list.contains('_') || abs(xCount - oCount) >= 2) {
+    // row check
+    for (v in 0..2) {
+        if (grid[v][0] == 'X' && grid[v][1] == 'X' && grid[v][2] == 'X') x = 1
+        if (grid[v][0] == 'O' && grid[v][1] == 'O' && grid[v][2] == 'O') o = 1
+    }
+
+    // column check
+    for (v in 0..2) {
+        if (grid[0][v] == 'X' && grid[1][v] == 'X' && grid[2][v] == 'X') x = 1
+        if (grid[0][v] == 'O' && grid[1][v] == 'O' && grid[2][v] == 'O') o = 1
+    }
+
+    // x and o count
+    for (i in 0..2) {
+        for (j in 0..2) {
+            if (grid[i][j] == 'X') xCount++
+            if (grid[i][j] == 'O') oCount++
+        }
+    }
+
+    // diagonal check
+    if (grid[0][0] == 'X' && grid[1][1] == 'X' && grid[2][2] == 'X') x = 1
+    if (grid[0][0] == 'O' && grid[1][1] == 'O' && grid[2][2] == 'O') o = 1
+    if (grid[0][2] == 'X' && grid[1][1] == 'X' && grid[2][0] == 'X') x = 1
+    if (grid[0][2] == 'O' && grid[1][1] == 'O' && grid[2][0] == 'O') o = 1
+
+    // evaluate
+    if (x == 1 && o == 1 && grid.contains(mutableListOf(' ')) || abs(xCount - oCount) >= 2) {
         result = "Impossible"
-    } else if (x == 0 && o == 0 && list.contains('_')) {
+    } else if (x == 0 && o == 0 && grid.contains(mutableListOf(' '))) {
         result = "Game not finished"
     } else if (x == 0 && o == 0 && xCount + oCount == 9) {
         result = "Draw"
-    } else if (x == 1 && o == 0 ) {
+    } else if (x == 1 && o == 0) {
         result = "X wins"
     } else if (x == 0 && o == 1) {
         result = "O wins"
     }
 
-    println(result)
+    return result
 }
